6f0c94f0dc5db8c51fe2f98253cb7b17ca1ba89a
YBSeok <112086285+YBSeok@users.noreply.github.com>
2025-06-18 03:01:21 +0900

    chore: project install

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PuppyMode-Front
+
 PuppyMode Front
+
+### 📦 주요 기술 요약
+
+- **버전:** 1.0.0
+- **코어:** React Native, TypeScript
+- **상태 관리:** React Query, Zustand
+- **스타일링:** Emotion
+- **패키지 매니저:** npm
+- **빌드:** Webpack, ESBuild
+- **CI/CD:** GitHub Actions
+
+---
---
+++ app/_layout.tsx
@@ -0,0 +1,33 @@
+import {useColorScheme} from '@/hooks/useColorScheme';
+import theme from '@/styles/theme';
+import {ThemeProvider as EmotionThemeProvider} from '@emotion/react';
+import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
+import {useFonts} from 'expo-font';
+import {Stack} from 'expo-router';
+import {StatusBar} from 'expo-status-bar';
+import 'react-native-reanimated';
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <EmotionThemeProvider theme={theme}>
+        <Stack>
+          <Stack.Screen name='(tabs)' options={{headerShown: false}} />
+          <Stack.Screen name='+not-found' />
+        </Stack>
+
+        <StatusBar style='auto' />
+      </EmotionThemeProvider>
+    </ThemeProvider>
+  );
+}
