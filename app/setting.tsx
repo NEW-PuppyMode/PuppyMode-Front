29ee4edd54e583489f4c3553c3dc4a4d70f9b123
Junhyeok <cjh64866@naver.com>
2025-08-29 04:22:43 +0900

    feat: setting header 생성

--- app/setting.tsx
+++ app/setting.tsx
@@ -1,9 +1,38 @@
 import SettingBtn from '@/components/page/setting/SettingBtn';
-import { StyleSheet, Text, View } from 'react-native';
+import { router } from 'expo-router';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Setting = () => {
   return (
     <View style={styles.container}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginTop: 48,
+          padding: 16,
+          width: '100%',
+          height: 60,
+        }}
+      >
+        <TouchableOpacity onPress={() => router.back()}>
+          <Image
+            source={require('@/assets/icons/test/ic_back.svg')}
+            style={{ margin: 10 }}
+          />
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: '600',
+            lineHeight: 28,
+            color: '#282828',
+          }}
+        >
+          설정
+        </Text>
+        <View style={{ width: 24 }} />
+      </View>
       <SettingBtn title='이용약관' onPress={() => {}} />
       <SettingBtn title='개인정보 처리방침' onPress={() => {}} />
       <SettingBtn title='탈퇴하기' onPress={() => {}} />
