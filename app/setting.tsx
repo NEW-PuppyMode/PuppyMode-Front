900745ff7c4ab263c18560fbc11442bbfdc29def
Junhyeok <cjh64866@naver.com>
2025-09-09 15:40:46 +0900

    feat: 설정 페이지 modal UI 구현

--- app/setting.tsx
+++ app/setting.tsx
@@ -1,8 +1,21 @@
+import DefaultModal from '@/components/common/DefaultModal';
+import PolicyModal from '@/components/page/setting/PolicyModal';
 import SettingBtn from '@/components/page/setting/SettingBtn';
+import { POLICY_MESSAGES } from '@/constants/messages';
+import { useAuth } from '@/contexts/AuthContext';
 import { router } from 'expo-router';
+import { useState } from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Setting = () => {
+  const [policyModalVisible, setPolicyModalVisible] = useState(false);
+  const [policyModalType, setPolicyModalType] = useState<
+    'terms_of_uses' | 'privacy_policies'
+  >('terms_of_uses');
+
+  const [signOutModalVisible, setSignOutModalVisible] = useState(false);
+
+  const { logout } = useAuth();
   return (
     <View style={styles.container}>
       <View
@@ -34,19 +47,79 @@ const Setting = () => {
         </Text>
         <View style={{ width: 24 }} />
       </View>
-      <SettingBtn title='이용약관' onPress={() => {}} />
-      <SettingBtn title='개인정보 처리방침' onPress={() => {}} />
+
+      <SettingBtn
+        title='이용약관'
+        onPress={() => {
+          setPolicyModalVisible(true);
+          setPolicyModalType('terms_of_uses');
+        }}
+      />
+      <SettingBtn
+        title='개인정보 처리방침'
+        onPress={() => {
+          setPolicyModalVisible(true);
+          setPolicyModalType('privacy_policies');
+        }}
+      />
       <SettingBtn
         title='탈퇴하기'
         onPress={() => {
           router.push('/delete_account');
         }}
       />
-      <SettingBtn title='로그아웃' onPress={() => {}} />
+      <SettingBtn
+        title='로그아웃'
+        onPress={() => {
+          setSignOutModalVisible(true);
+        }}
+      />
       <View style={styles.view}>
         <Text style={styles.text}>앱 버전</Text>
         <Text style={styles.text}>1.0.0</Text>
       </View>
+
+      <PolicyModal
+        title={
+          policyModalType === 'terms_of_uses' ? '이용약관' : '개인정보 처리방침'
+        }
+        content={POLICY_MESSAGES[policyModalType]}
+        visible={policyModalVisible}
+        setPolicyModalVisible={setPolicyModalVisible}
+      />
+      <DefaultModal
+        visible={signOutModalVisible}
+        setVisible={setSignOutModalVisible}
+      >
+        <View className='justify-between p-[15px] pt-[35px] w-[336px] h-[154px] bg-white rounded-[10px]'>
+          <Text className='text-[#555] text-center font-semibold text-[18px]'>
+            정말 로그아웃을 하시겠습니까?
+          </Text>
+
+          <View className='flex-row justify-between'>
+            <TouchableOpacity
+              onPress={() => {
+                setSignOutModalVisible(false);
+                logout();
+                router.replace('/signin');
+              }}
+              className='justify-center items-center w-[148px] h-[48px] border-[1px] border-[#0FD380] bg-white rounded-[5px]'
+            >
+              <Text className='text-[#0FD380] font-medium text-[12px]'>
+                확인
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className='justify-center items-center w-[148px] h-[48px] bg-[#0FD380] rounded-[5px]'
+              onPress={() => setSignOutModalVisible(false)}
+            >
+              <Text className='text-[#F2FFF4] font-medium text-[12px]'>
+                취소
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </DefaultModal>
     </View>
   );
 };
