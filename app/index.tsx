4467c6abd0064ad0fc342b5404cc82395c8ade1c
seunghyeonKang <number42isuser@gmail.com>
2026-03-06 08:09:50 +0900

    feat: [index.tsx] 토큰이 있으면 home으로 바로 이동

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,31 @@
+import { KEYS } from '@/constants/storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Redirect } from 'expo-router';
+import { useEffect, useState } from 'react';
+
 import 'react-native-gesture-handler';
 import 'react-native-reanimated';
+
 export default function Index() {
+  const [hasToken, setHasToken] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    async function checkToken() {
+      const token = await AsyncStorage.getItem(KEYS.ACCESS_TOKEN);
+      console.log('맨처음 인덱스에서 token:', token);
+      setHasToken(!!token);
+    }
+
+    checkToken();
+  }, []);
+
+  if (hasToken === null) {
+    return null;
+  }
+
+  if (hasToken) {
+    return <Redirect href='/home' />;
+  }
+
   return <Redirect href='/signin' />;
 }
